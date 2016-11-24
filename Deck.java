2440266a9c3f453f939df718a94ff9a54c343c56
erikajansson1 <erika_jansson1@hotmail.com>
2016-11-24 22:53:44 +0100

    Lite testkodande för en kortlek.....

---
+++ Deck.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Deck {
+    private Card card;
+    private int amountOfCards;
+
+    publiv Deck (Card card, int amountOfCards) {
+	this.card = card;
+	this.amountOfCards = amountOfCards;
+    }
+
+    public Card getCard() {
+	return card;
+    }
+
+}
