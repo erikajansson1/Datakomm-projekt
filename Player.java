4eec0762176dc9d7f29627ba543d70863fafbcb5
DeeAwesomeOne <Desiree.Bjorkman.4477@student.uu.se>
2016-11-27 16:28:52 +0100

    Just a plain shell.

---
+++ Player.java
@@ -0,0 +1,11 @@
+
+
+//Player class that should be given to each client
+/*TODO: I think we need to place this a superclass, where we split
+ * Players from game master.
+*/
+public class Player {
+	public Deck playerDeck;
+	
+	
+}
