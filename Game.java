007805905943da3eabedcf2098fdc1a361636cd5
DeeAwesomeOne <Desiree.Bjorkman.4477@student.uu.se>
2016-11-27 16:29:15 +0100

    Start of game, unsure if we should have a main in this of just rules and steps

---
+++ Game.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+//This is the general game concept
+public class Game {
+	public Deck gameDeck;
+	//public player[] gamePlayers
+	//public master gameMaster?
+	
+	//Intiate game by giving out cards and select who start first
+	public void startGame(){
+		return;
+	}
+	
+	//Next player should be able to place a cards
+	//TODO: I don't know if this is needed
+	public void nextplayer() {
+		return;
+	}
+	
+	//Inform player who lost and resign it from game
+	public void looseGame(){ // Should have decided player that lost
+		return;
+	}
+	
+}
