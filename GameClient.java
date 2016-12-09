2dc3111e7100ae385494491878252b24f58c5b07
DeeAwesomeOne <Desiree.Bjorkman.4477@student.uu.se>
2016-12-09 22:42:40 +0100

    Need to rethink start game, some svengelska borta, propotion to todo, found error

--- GameClient.java
+++ GameClient.java
@@ -44,6 +44,11 @@ public class GameClient {
 	    //BEGINNING OF GAME
 	    
 	    serverGame.startGame(playerNo);
+	    
+	    //Gives out information about how many player. We need functions to get strings that get aliases
+	    // and everyones Deck size.
+	   
+	    
 	    //START VALUES
 	    long startTime;
 	    long hitTime;
@@ -56,27 +61,27 @@ public class GameClient {
 	    boolean myRound = false; 
 	    boolean checkVar = false;
 
-	    //>>>>STOR LOOP: har ska vi egentligen ha en check att spelet inte ar slut
+	    //>>>>STOR LOOP: Here we should also have a check if the game is finished or not
 	    while (serverGame.getPlayer(playerNo).getPlayerRank() == -1) {
 		backup.update(serverGame);
 		serverGame.setReadyValue(playerNo, false);
 		//Check if it's possible to hit
-		canHit = serverGame.timeToHit(); //TODO: fkn for checking if its hit the dick time 
+		canHit = serverGame.timeToHit(); 
 
 		//Display board
-		System.out.printf("\033[2J\033[;H");
+		System.out.printf("\033[2J\033[;H"); //Why do we print out this?
 		System.out.println(serverGame.displayBoard());
 		System.out.println("Current dick size " + serverGame.getDeckSize());
 		System.out.println(checkVar);
 
-		//Kolla ifall det ar spelarens tur
+		//Check if it's current player's time to lay card
 		if (serverGame.whoseTurn() == playerNo) { myRound = true; }
 		else { myRound = false; }
 		
 
 		    
-		serverGame.setReadyValue(playerNo, false);
-
+		serverGame.setReadyValue(playerNo, false); 
+		
 		//Let the player make its move
 		checkVar = userAction(serverGame, playerNo, round, canHit,myRound);
 
@@ -84,7 +89,15 @@ public class GameClient {
 
 		
 		//TODO UPDATE THE WHOLE GAME STATUS TBH. Kolla sa att alla har gjort sitt och att losern har fatt kort
+				//Completely lost in how we compare players to eachother from this loop
+		
 		//TODO: kolla ifall personen fortfarande deltar i spelet eller har vunnit.
+			//Take rankWhenFinished from player in startGame function
+			//When cards on hand == 0, change the rankWhenFinished in player (need function)
+			//Put in while a statement about rankWhenFinished. If we do this we can have to
+			// state, 
+			// 1.You are still playing and go though this loop
+			// 2.You won but still se the outprint in another loop
 		
 	       
 		
@@ -110,6 +123,7 @@ public class GameClient {
 	}
 
 	//TODO Add ability to passivly look at game after winning.
+		//Look above
     }
 
 
@@ -139,15 +153,15 @@ public class GameClient {
 	    
 	    if (canHit) {
 		switch(answer) {
-		case "h": serverGame.handleRightHit(playerNo, round); break;
+		case "h": serverGame.handleRightHit(playerNo, round); break; 
 		case "c": card = serverGame.tryToLayCard(playerNo, round); break;
 		default: break;
 		}
 	    } 
 	    else {
 		switch(answer) {
-		case "h": actionMessage = serverGame.handleWrongHit(playerNo, round); break;
-		case "c": card = serverGame.tryToLayCard(playerNo, round); break;
+		case "h": actionMessage = serverGame.handleWrongHit(playerNo, round); break; //what happens with message?
+		case "c": card = serverGame.tryToLayCard(playerNo, round); break; //Creates Error, IndexOutOfBoundsException
 		default: break;
 		}
 	    }
